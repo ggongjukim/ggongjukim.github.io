0c380db6f0044f9078a9e80b0772be064cc05e87
ggongjukim <chaeniiii@magiclab.kr>
2022-06-08 16:32:58 +0900

    Baekjoon1920

---
+++ _pythonfile/[Baekjoon]1920.py
@@ -0,0 +1,31 @@
+#문제
+#N개의 정수가 주어졌을때 x정수가 존재하는지 알아내는 프로그램
+
+#입력
+#첫줄: N
+#둘째줄 : N개의 정수(배열 A)
+#셋째줄 : M
+#넷째줄 : M개의 수
+
+#결과
+#M개의 수가 배열 A에 존재하면 1, 존재하지 않으면 0
+
+n = int(input())
+A_arr = list(map(int,input().split()))
+m = int(input())
+M_arr = list(map(int,input().split()))
+
+A_arr.sort()
+def binarySearch(arr, target, start, end):
+  if start > end:
+    return 0
+  mid = (start+end)//2
+  if arr[mid] == target:
+    return 1
+  elif arr[mid] > target:
+    return binarySearch(arr, target, start, mid-1)
+  else:
+    return binarySearch(arr, target, mid+1, end)
+
+for i in M_arr:
+  print(binarySearch(A_arr,i,0,n-1))
