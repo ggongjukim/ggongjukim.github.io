9f2a9249177277194b3c91161a15af61c4484543
ggongjukim <chaeniiii@magiclab.kr>
2022-07-05 13:43:21 +0900

    Update [Baekjoon]1966프린터큐-실버3.py

--- _pythonfile/[Baekjoon]1966프린터큐-실버3.py
+++ _pythonfile/[Baekjoon]1966프린터큐-실버3.py
@@ -12,25 +12,56 @@
 
 import sys
 from collections import deque
-dic ={}
-temp = list(map(int,input().split(' ')))
-print(temp)
+countarr=[]
+def test():
+  arr = []
+  count = 0
+  n,m = list(map(int,input().split(' ')))
+  temp = list(map(int,input().split(' ')))
+  # print(temp)
 
-for k,v in enumerate(temp):
-  dic[k] = v
-print(dic)
+  for k,v in enumerate(temp):
+    arr.append([v,k])
+  # print(arr)
 
+  while arr:
+    if max(arr)[0] == arr[0][0]:
+      count += 1
+      if m == arr[0][1]:
+        countarr.append(count)
+        break
+      arr = arr[1:]
+      # print("pop", arr)
 
-i = 0
-while dic:
+    else :
+      arr = arr[1:] + [arr[0]]
+      # print("뒤에 붙이기", arr)
 
-  if dic[i] == max(dic.values()):
-    tmp = deque()
-    tmp = list(dic.keys())
-    print("팝", tmp)
+T = int(input())
+for _ in range(T):
+  test()
 
-    dic.pop(tmp.popleft())
-  # else:
-  #   tmp = dic[i]
+for i in countarr:
+  print(i)
 
-  i += 1
+# #다른 풀이
+# from collections import deque
+#
+# t = int(input())
+# for _ in range(t):
+#     n, m = map(int, input().split())
+#     print_q = deque(list(map(int, input().split())))
+#     order_q = deque([[0] for _ in range(n)])
+#
+#     answer = []
+#     order_q[m] = 1
+#     while print_q:
+#         print_now = print_q.popleft()
+#         order_now = order_q.popleft()
+#
+#         if print_q and print_now < max(print_q):
+#             print_q.append(print_now)
+#             order_q.append(order_now)
+#         else:
+#             answer.append(order_now)
+#     print(answer.index(1) + 1)
