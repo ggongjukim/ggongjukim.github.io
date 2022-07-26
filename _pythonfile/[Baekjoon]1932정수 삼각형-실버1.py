5c21d98de5b16a1e2680e98c2519ecc5cb891b05
ggongjukim <chaeniiii@magiclab.kr>
2022-07-26 20:34:26 +0900

    Update [Baekjoon]1932정수 삼각형-실버1.py

--- _pythonfile/[Baekjoon]1932정수 삼각형-실버1.py
+++ _pythonfile/[Baekjoon]1932정수 삼각형-실버1.py
@@ -1,20 +1,29 @@
 N = int(input())
 graph =[]
-dp =[]
+
 for _ in range(N):
   graph.append(list(map(int,input().split(' '))))
-print(len(graph))
+
+dp =[graph[0]]
+countdp = [graph[0]]
+
+# print(dp, countdp)
 
 for i in range(1,len(graph)):
-  for j in range(len(i)):
+  countdp.append([])
+
+  for j in range(len(graph[i])):
 
     #윗줄 한칸 앞이랑 더하기
     if j-1 < 0:
-      pass #다음줄로 넘어가기
+      before = 0
     else:
-      dp.append(graph[i][j] + dp[i-1][j-1])
+      before = countdp[i-1][j-1]
 
     if j>= len(graph[i-1]): #윗줄 길이 보다 크면
-      pass# 다음 줄로 넘어가기
+      after = 0
     else:
-      dp.append(graph[i][j] + dp[i-1][j])
+      after = countdp[i-1][j]
+
+    countdp[i].append(max(before,after)+graph[i][j])
+print(max(countdp[-1]))
